a42a612162a2d7fc99b8449df7019f2c5c483ed1
Anastasia Art <art-nastya@inbox.ru>
2019-06-08 00:31:39 +0300

    добавляю исходники

--- src/index.js
+++ src/index.js
@@ -1,2 +1,42 @@
 import "./scss/main.scss";
-console.log("hello, world");
+import "bootstrap/js/dist/collapse";
+import Vue from 'vue';
+
+
+Vue.directive('scroll', {
+    inserted: function (el, binding) {
+        let f = function (evt) {
+            if (binding.value(evt, el)) {
+                window.removeEventListener('scroll', f)
+            }
+        }
+        window.addEventListener('scroll', f)
+    }
+})
+
+// main
+new Vue({
+    el: '#app',
+    data: {
+        show: false
+    },
+
+    methods: {
+        handleScroll(evt, el) {
+            if (window.scrollY > 50) {
+                el.setAttribute(
+                    'style',
+                    'background-color: #222222'
+                )
+            }
+            return window.scrollY > 100
+        },
+
+        scrollMeTo(refName) {
+            var element = this.$refs[refName];
+            var top = element.offsetTop;
+
+            window.scrollTo(0, top);
+        },
+    }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,23 @@
 // Webpack v4
 const path = require('path');
+const jquery =  require("jquery");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
+const CopyWebpackPlugin= require('copy-webpack-plugin');
+
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[hash].js'
+    },
+    resolve: {
+        // extensions: ['.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
     },
     module: {
         rules: [
@@ -21,46 +30,57 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader','postcss-loader', 'sass-loader']
+                use:  [  'style-loader', MiniCssExtractPlugin.loader,
+                {
+                    loader: "css-loader",
+                    options: {
+                        url: false
+                    }
+                },'postcss-loader', 'sass-loader']
             },
             {
-                test: /images[\\\/].+\.(png|jpe?g|svg)$/i,
+                test:/\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
                 use: [{
                     loader: 'file-loader',
                     options: {
-                        name: 'images/[name][hash].[ext]'
+                        name: '[name].[ext]',
+                        outputPath: 'dist/fonts/'
                     }
-                }, {
-                    loader: 'image-webpack-loader',
-                    options: {
-                        mozjpeg: {
-                            progressive: true,
-                            quality: 70
-                        }
-                    }
-                },
-            ],
-        }, {
-            test: /fonts[\\\/].+\.(eot|svg|ttf|woff|woff2)$/i,
-            use: {
-                loader: 'file-loader',
-                options: {
-                    name: 'fonts/[name][hash].[ext]'
-                }
+                }]
             },
-        }]
+        ]
     },
+
     plugins: [
         new CleanWebpackPlugin(),
+
         new MiniCssExtractPlugin({
             filename: 'style.[contenthash].css',
         }),
+
         new HtmlWebpackPlugin({
             inject: false,
             hash: true,
             template: './src/index.html',
             filename: 'index.html'
         }),
-        new WebpackMd5Hash()
-    ]
+
+        new WebpackMd5Hash(),
+        new CopyWebpackPlugin([
+            {
+                from: './src/images',
+                to: './images'
+            },
+            {
+                from: './src/fonts',
+                to: './fonts'
+            }
+        ])
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 9000,
+        compress: true,
+        open: true
+    }
 };
